ee74ffd70fc79448c064b0ddc19c0cb113668b41
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-04 15:07:34 +0530

    ok

--- src/components/LevelSelector.js
+++ src/components/LevelSelector.js
@@ -3,39 +3,45 @@ import React from "react";
 const LevelSelector = ({ level, setLevel }) => {
   return (
     <div className="levels_container">
-      <label>
-        <input
-          type="radio"
-          name="level"
-          value="easy"
-          id="easy"
-          checked={level === "easy"}
-          onChange={() => setLevel("easy")}
-        />{" "}
-        <span>Easy</span>
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="level"
-          value="normal"
-          id="normal"
-          checked={level === "normal"}
-          onChange={() => setLevel("normal")}
-        />{" "}
-        <span>Normal</span>
-      </label>
-      <label>
-        <input
-          type="radio"
-          name="level"
-          value="hard"
-          id="hard"
-          checked={level === "hard"}
-          onChange={() => setLevel("hard")}
-        />{" "}
-        <span>Hard</span>
-      </label>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="level"
+            value="easy"
+            id="easy"
+            checked={level === "easy"}
+            onChange={() => setLevel("easy")}
+          />{" "}
+          <span>Easy</span>
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="level"
+            value="normal"
+            id="normal"
+            checked={level === "normal"}
+            onChange={() => setLevel("normal")}
+          />{" "}
+          <span>Normal</span>
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="level"
+            value="hard"
+            id="hard"
+            checked={level === "hard"}
+            onChange={() => setLevel("hard")}
+          />{" "}
+          <span>Hard</span>
+        </label>
+      </div>
     </div>
   );
 };
