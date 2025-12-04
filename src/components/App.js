f9fa4ec3e847404072786c7f298b819ee774580c
MrAhmed0702 <amochi670@rku.ac.in>
2025-12-04 14:33:45 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import '../styles/App.css';
 import GameBoard from './GameBoard';
 import LevelSelector from './LevelSelector';
 import Scoreboard from './Scoreboard';
